e7aa6dbb89ae28262f006bb0d99627eb1ca01980
Panayoti Kourkoumelis <72515650+Yoti72@users.noreply.github.com>
2024-04-10 10:33:32 -0400

    fixed template service null exception
    added some html structure

--- Data/TemplateService.cs
+++ Data/TemplateService.cs
@@ -3,5 +3,10 @@ namespace WebBuilder.Data
     public class TemplateService
     {
         public TemplateModel CurrentTemplate { get; set; }
+
+        public TemplateService()
+        {
+            CurrentTemplate = new TemplateModel();
+        }
     }
 }
